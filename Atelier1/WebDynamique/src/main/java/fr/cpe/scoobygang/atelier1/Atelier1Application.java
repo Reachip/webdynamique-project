556bd8ff47226d426d12211b4a387c617d64b6c2
Nathan Guillemette <nathan.guillemette73@gmail.com>
2024-05-03 08:40:48 +0200

    create Web Dynamique project

---
+++ Atelier1/WebDynamique/src/main/java/fr/cpe/scoobygang/atelier1/Atelier1Application.java
@@ -0,0 +1,13 @@
+package fr.cpe.scoobygang.atelier1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atelier1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atelier1Application.class, args);
+	}
+
+}
