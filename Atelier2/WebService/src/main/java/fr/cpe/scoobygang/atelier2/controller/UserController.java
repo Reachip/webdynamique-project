642ca0c0dc4c7317474293238701d429d5fdfff5
Rached Mejri <r.mejri74100@gmail.com>
2024-05-21 22:52:38 +0200

    balance dans la request

--- Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/controller/UserController.java
+++ Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/controller/UserController.java
@@ -30,8 +30,8 @@ public class UserController {
     }
 
     @PostMapping(value="/register")
-    public void addUser(@RequestBody User user) {
-        userService.addUser(user);
+    public void addUser(@RequestBody UserRequest user) {
+        userService.addUser(UserMapper.INSTANCE.userRequestToUser(user));
     }
 
     @PostMapping(value = "/login")
