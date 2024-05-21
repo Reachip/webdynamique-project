d740641133131f232d7803fb57ea271f544cfa03
Nathan Guillemette <nathan.guillemette73@gmail.com>
2024-05-21 10:52:27 +0200

    ajout get all users

--- Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/model/User.java
+++ Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/model/User.java
@@ -1,8 +1,6 @@
 package fr.cpe.scoobygang.atelier2.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "users")
 public class User {
-
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String surname;
     private String password;
