e7c10b2b6276ab29696d98f3a4272bf59ca6fe20
Nathan Guillemette <nathan.guillemette73@gmail.com>
2024-05-21 13:49:25 +0200

    generate token

--- Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/model/User.java
+++ Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/model/User.java
@@ -12,7 +12,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String name;
     private String surname;
     private String password;
--- Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/service/UserService.java
+++ Atelier2/WebService/src/main/java/fr/cpe/scoobygang/atelier2/service/UserService.java
@@ -2,10 +2,15 @@ package fr.cpe.scoobygang.atelier2.service;
 
 import fr.cpe.scoobygang.atelier2.model.User;
 import fr.cpe.scoobygang.atelier2.repository.UserRepository;
+import fr.cpe.scoobygang.atelier2.security.JWT;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -22,5 +27,16 @@ public class UserService {
         Iterable<User> iterable = userRepository.findAll();
         return StreamSupport.stream(iterable.spliterator(), false)
                 .collect(Collectors.toList());
+    }
+
+    public Optional<JWT> login(String surname, String password) {
+        User existingUser = userRepository.findBySurname(surname);
+
+        if (existingUser == null || !Objects.equals(existingUser.getPassword(), password)) {
+            return Optional.empty();
+        }
+
+        JWT jwt = JWT.fromUser(existingUser);
+        return Optional.of(jwt);
     }
 }
