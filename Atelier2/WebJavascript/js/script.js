672e2494dce7534fd5c4daafcdaf1ef6b2c3bf22
baptiste-gris <gris.baptiste@gmail.com>
2024-05-21 16:58:25 +0200

    add: register page

--- Atelier2/WebJavascript/js/script.js
+++ Atelier2/WebJavascript/js/script.js
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const links = [
     { text: "<i class=\"fa-solid fa-house\"></i> Accueil", href: "index.html" },
-    { text: "<i class=\"fa-solid fa-list\"></i> Liste des cartes", href: "cards.html" },
-    { text: "<i class=\"fa-solid fa-layer-group\"></i> Mes cartes", href: "cardList.html" }
+    { text: "<i class=\"fa-solid fa-list\"></i> Liste des cartes", href: "cardList.html" },
+    { text: "<i class=\"fa-solid fa-layer-group\"></i> Mes cartes", href: "cards.html" }
   ];
 
   links.forEach(link => {
@@ -39,17 +39,19 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const headerBadgesContainer = document.createElement("div");
-  headerBadgesContainer.classList.add("header-badges-container");
+  headerBadgesContainer.classList.add("badges-container");
 
   const usernameBadge = document.createElement("a");
-  usernameBadge.classList.add("header-badge");
+  usernameBadge.classList.add("badge");
+  usernameBadge.classList.add("badge-primary-dark");
   usernameBadge.classList.add("pointer");
   usernameBadge.classList.add("no-decoration");
   usernameBadge.innerHTML = "<i class=\"fa-solid fa-right-to-bracket\"></i> Se connecter";
   usernameBadge.href = "login.html";
 
   const balanceBadge = document.createElement("div");
-  balanceBadge.classList.add("header-badge");
+  balanceBadge.classList.add("badge");
+  usernameBadge.classList.add("badge-primary-dark");
   balanceBadge.innerHTML = "Solde : 100,00 €";
   balanceBadge.classList.add("hidden");
   
