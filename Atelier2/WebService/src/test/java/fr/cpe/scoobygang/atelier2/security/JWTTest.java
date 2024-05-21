8512af4c512c3d7842f97587e584e464d5eacdd8
Rached Mejri <r.mejri74100@gmail.com>
2024-05-21 14:31:18 +0200

    mise en place d'un service pour le JWT

--- Atelier2/WebService/src/test/java/fr/cpe/scoobygang/atelier2/security/JWTTest.java
+++ Atelier2/WebService/src/test/java/fr/cpe/scoobygang/atelier2/security/JWTTest.java
@@ -3,10 +3,16 @@ package fr.cpe.scoobygang.atelier2.security;
 import fr.cpe.scoobygang.atelier2.model.User;
 import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class JWTTest {
+    @Autowired
+    private JWTService jwtService;
+
     @Test
     void fromUserIsOK() {
         final User user = new User();
@@ -14,7 +20,7 @@ class JWTTest {
         user.setPassword("123");
         user.setSurname("Mejri");
 
-        Assert.notNull(JWT.fromUser(user));
+        Assert.notNull(jwtService.fromUser(user));
     }
 
     @Test
@@ -24,14 +30,14 @@ class JWTTest {
         user.setPassword("123");
         user.setSurname("Mejri");
 
-        String jwt = JWT.fromUser(user).getToken();
-        assertTrue(JWT.isOk(jwt));
+        String jwt = jwtService.fromUser(user).getToken();
+        assertTrue(jwtService.isOk(jwt));
     }
 
 
     @Test
     void isOkIsNotOk() {
         String badJWT = "ccJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJSYWNoZWQiLCJleHAiOjE3MTYyOTUwMjd9.VqEaepmMAIf5ySFFXDu09YsIlLqerCZMYmmeKK3AsQG8CHUJgroMrBFcgbFD4M4A1IO7hZOnFHvfHOdVjYzbOw";
-        assertFalse(JWT.isOk(badJWT));
+        assertFalse(jwtService.isOk(badJWT));
     }
 }
