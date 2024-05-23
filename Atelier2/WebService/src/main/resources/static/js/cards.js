7c1038c85a7461c15ec3ae57a45d1301b5295f24
baptiste-gris <gris.baptiste@gmail.com>
2024-05-23 18:10:00 +0200

    wip: sell cancel

--- Atelier2/WebService/src/main/resources/static/js/cards.js
+++ Atelier2/WebService/src/main/resources/static/js/cards.js
@@ -1,4 +1,4 @@
-import {Alert, showAlert} from './alerts.js';
+import { Alert, showAlert } from './alerts.js';
 
 document.addEventListener("DOMContentLoaded", function () {
     const userToken = localStorage.getItem("scoobycards-user-token");
@@ -56,14 +56,26 @@ document.addEventListener("DOMContentLoaded", function () {
                                             const previewEnergy = document.querySelector('#preview-energy');
                                             const previewDefense = document.querySelector('#preview-defense');
                                             const previewAttack = document.querySelector('#preview-attack');
-                                            const previewRadios = document.querySelector('#preview-radios');
+                                            const previewButton = document.querySelector('#preview-button');
 
                                             var radioInputs = ``;
-                                            let isFirstRadio = true;
-                                            stores.forEach(store => {
-                                                radioInputs += `<input type="radio" id="store-radio-${store.id}" name="store" value="${store.id}" data-card="${card.id}" ${isFirstRadio ? "checked" : ""}><label for="${store.id}">${store.name}</label><br>`;
-                                                isFirstRadio = false;
-                                            });
+                                            var button;
+                                            if (parseInt(card.storeId) > 0) {
+                                                button = `<button class="button button-primary button-sell">
+                                                <i class="fa-solid fa-cart-arrow-down"></i> Vendre
+                                            </button>`;
+
+                                                let isFirstRadio = true;
+                                                stores.forEach(store => {
+                                                    radioInputs += `<input type = "radio" id="store-radio-${store.id}" name = "store" value = "${store.id}" data-card="${card.id}" ${isFirstRadio ? "checked" : ""}><label for="${store.id}">${store.name}</label><br>`;
+                                                    isFirstRadio = false;
+                                                });
+                                            }
+                                            else {
+                                                button = `<button class="button button-primary button-sell-cancel">
+                                                <i class="fa-solid fa-cart-arrow-down"></i> Annuler la vente
+                                            </button>`;
+                                            }
 
                                             rows.forEach(row => {
                                                 row.querySelector(".button-details").addEventListener("click", () => {
@@ -76,95 +88,11 @@ document.addEventListener("DOMContentLoaded", function () {
                                                     previewEnergy.textContent = row.children[5].textContent;
                                                     previewDefense.textContent = row.children[6].textContent;
                                                     previewAttack.textContent = row.children[7].textContent;
-                                                    previewRadios.innerHTML = radioInputs;
+                                                    previewButton.innerHTML = radioInputs + button;
 
                                                     preview.classList.remove('hidden');
                                                 });
-                                            });
-
-                                            const sellButton = row.querySelector(".button-sell");
-                                            console.log("row", row)
-                                            console.log("sell btn", sellButton)
-                                            let callback = (cardId, storeId) => {
-                                                fetch("http://127.0.0.1:8080/store/sell", {
-                                                    method: "POST",
-                                                    body: JSON.stringify({
-                                                        cardId: cardId,
-                                                        storeId: storeId
-                                                    }),
-                                                    headers: {
-                                                        "Authorization": "Bearer " + userToken,
-                                                        "Content-type": "application/json; charset=UTF-8"
-                                                    }
-                                                })
-                                                    .then(response => {
-                                                        if (response.ok) {
-                                                            showAlert(Alert.SUCCESS, "Carte ajoutée à la vente.");
-                                                            setTimeout(() => {
-                                                                window.location.reload();
-                                                            }, 2000);
-                                                        } else {
-                                                            showAlert(Alert.ERROR, "Échec de la mise en vente de la carte.");
-                                                        }
-                                                    })
-                                                    .catch(error => {
-                                                        console.error("Fetch error:", error);
-                                                        showAlert(Alert.ERROR, "Une erreur est survenue.");
-                                                    });
-                                            }
-
-                                            if (card.storeId != 0) {
-                                                console.log("card", card)
-                                                sellButton.innerText = "Annuler la vente"
-
-                                                callback = (cardId, storeId) => {
-                                                    fetch("http://127.0.0.1:8080/store/sell/cancel", {
-                                                        method: "POST",
-                                                        body: JSON.stringify({
-                                                            cardId: cardId,
-                                                            storeId: storeId
-                                                        }),
-                                                        headers: {
-                                                            "Authorization": "Bearer " + userToken,
-                                                            "Content-type": "application/json; charset=UTF-8"
-                                                        }
-                                                    })
-                                                        .then(response => {
-                                                            if (response.ok) {
-                                                                showAlert(Alert.SUCCESS, "Carte retirée de la vente.");
-                                                                setTimeout(() => {
-                                                                    window.location.reload();
-                                                                }, 2000);
-                                                            } else {
-                                                                showAlert(Alert.ERROR, "Échec de la supression de la vente de la carte.");
-                                                            }
-                                                        })
-                                                        .catch(error => {
-                                                            console.error("Fetch error:", error);
-                                                            showAlert(Alert.ERROR, "Une erreur est survenue.");
-                                                        });
-                                                }
-                                            }
-
-                                            sellButton.addEventListener("click", function (event) {
-                                                let storeId, cardId;
-                                                const radios = event.target.parentElement.querySelectorAll("input[type='radio']");
-
-                                                for (let i = 0; i < radios.length; i++) {
-                                                    const radio = radios[i];
-                                                    if (radio.checked) {
-                                                        storeId = radio.value;
-                                                        cardId = radio.getAttribute("data-card");
-                                                        break;
-                                                    }
-                                                }
-
-                                                callback(storeId, cardId);
-                                            });
-                                        }
-                                    });
-                                }
-                            });
+                                            });                       });
                     }).catch(error => {
                         console.error("Error when parsing JSON:", error);
                     });
