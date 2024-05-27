c4150e4a4d2544c0b3beea35aa1af410e2d7c8b2
baptiste-gris <gris.baptiste@gmail.com>
2024-05-27 11:46:38 +0200

    init: microservices

---
+++ Atelier3/api-transaction-microservice/src/main/java/fr/cpe/scoobygang/atelier3/api_transaction_microservice/ApiTransactionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.cpe.scoobygang.atelier3.api_transaction_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransactionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransactionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Atelier3/api-user-microservice/src/main/java/fr/cpe/scoobygang/atelier3/api_user_microservice/ApiUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.cpe.scoobygang.atelier3.api_user_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserMicroserviceApplication.class, args);
+	}
+
+}
