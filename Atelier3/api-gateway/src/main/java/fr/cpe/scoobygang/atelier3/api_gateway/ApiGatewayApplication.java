00e8017b8fe33e81a221532291043be3e094a5d3
Rached Mejri <r.mejri74100@gmail.com>
2024-05-27 17:29:46 +0200

    configuration de gateway

--- Atelier3/api-gateway/src/main/java/fr/cpe/scoobygang/atelier3/api_gateway/ApiGatewayApplication.java
+++ Atelier3/api-gateway/src/main/java/fr/cpe/scoobygang/atelier3/api_gateway/ApiGatewayApplication.java
@@ -3,11 +3,23 @@ package fr.cpe.scoobygang.atelier3.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route("transaction-route", r -> r.path("/transaction/**").uri("lb://api-transaction-microservice"))
+				.build();
 	}
 }
