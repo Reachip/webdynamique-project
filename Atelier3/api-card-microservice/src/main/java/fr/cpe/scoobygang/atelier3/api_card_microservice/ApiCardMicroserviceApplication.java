b3a38ca04237cedc78dc7b031c30f7907e6b42ff
JorisGarcia19 <garcia.joris@outlook.fr>
2024-05-27 16:51:49 +0200

    update properties card service

--- Atelier3/api-card-microservice/src/main/java/fr/cpe/scoobygang/atelier3/api_card_microservice/ApiCardMicroserviceApplication.java
+++ Atelier3/api-card-microservice/src/main/java/fr/cpe/scoobygang/atelier3/api_card_microservice/ApiCardMicroserviceApplication.java
@@ -3,9 +3,11 @@ package fr.cpe.scoobygang.atelier3.api_card_microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {
 		"fr.cpe.scoobygang.common.jwt",
 		"fr.cpe.scoobygang.common.resource",
