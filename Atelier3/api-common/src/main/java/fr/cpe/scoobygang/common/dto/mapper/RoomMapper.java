35dc9f3152fe06a520dac553a60691bf265081eb
Rached Mejri <r.mejri74100@gmail.com>
2024-05-28 18:26:50 +0200

    correction conflit

--- Atelier3/api-common/src/main/java/fr/cpe/scoobygang/common/dto/mapper/RoomMapper.java
+++ Atelier3/api-common/src/main/java/fr/cpe/scoobygang/common/dto/mapper/RoomMapper.java
@@ -1,6 +1,7 @@
 package fr.cpe.scoobygang.common.dto.mapper;
 
 import fr.cpe.scoobygang.common.dto.request.RoomCreateRequest;
+import fr.cpe.scoobygang.common.dto.response.RoomResponse;
 import fr.cpe.scoobygang.common.model.Room;
 import fr.cpe.scoobygang.common.model.User;
 import org.mapstruct.Mapper;
@@ -14,19 +15,6 @@ import java.util.List;
 public interface RoomMapper {
     RoomMapper INSTANCE = Mappers.getMapper( RoomMapper.class );
     Room roomToRoomCreateRequest(RoomCreateRequest room);
-
-    @Mapping(source = "owner", target = "userId", qualifiedByName = "ownerToUserId")
-    RoomCreateRequest roomCreateRequestToRoom(Room room);
-
-    List<RoomCreateRequest> roomCreateRequestsToRooms(List<Room> rooms);
-
-    @Named("ownerToUserId")
-    static int ownerToUserId(User owner) {
-        if (owner == null) {
-            return 0;
-        }
-        return owner.getId();
-    }
 
     RoomResponse roomToRoomResponse(Room room);
 
