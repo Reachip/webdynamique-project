bc9dba98244d943ef59779c28fa3a7dc408eab78
Rached Mejri <r.mejri74100@gmail.com>
2024-05-28 10:58:46 +0200

    pas d'Atelier3-Room

--- Atelier3-Room/api-common/src/main/java/fr/cpe/scoobygang/common/exceptions/UserChangePasswordException.java
+++
@@ -1,7 +0,0 @@
-package fr.cpe.scoobygang.common.exceptions;
-
-public class UserChangePasswordException extends Exception {
-    public UserChangePasswordException(String message) {
-        super(message);
-    }
-}
--- Atelier3-Room/api-common/src/main/java/fr/cpe/scoobygang/common/resource/Resource.java
+++
@@ -1,6 +0,0 @@
-package fr.cpe.scoobygang.common.resource;
-
-public interface Resource {
-    org.springframework.core.io.Resource load();
-}
-
