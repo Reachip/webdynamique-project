9627c4e871fe3b81283dbb8778b25ce9d4c81c5b
Rached Mejri <r.mejri74100@gmail.com>
2024-05-28 11:06:15 +0200

    micro service Room

---
+++ Atelier3/api-room-microservice/src/main/java/fr/cpe/scoobygang/atelier3/api_room_microservice/ApiRoomMicroserviceApplication.java
@@ -0,0 +1,24 @@
+package fr.cpe.scoobygang.atelier3.api_room_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {
+		"fr.cpe.scoobygang.common.jwt",
+		"fr.cpe.scoobygang.common.resource",
+		"fr.cpe.scoobygang.common.repository",
+		"fr.cpe.scoobygang.common.config"
+})
+@EntityScan("fr.cpe.scoobygang.common.model")
+public class ApiRoomMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRoomMicroserviceApplication.class, args);
+	}
+
+}
