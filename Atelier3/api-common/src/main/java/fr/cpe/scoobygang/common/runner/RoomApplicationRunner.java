45d003127722500f74017d54be4e6eb9beb90161
Rached Mejri <r.mejri74100@gmail.com>
2024-05-28 15:08:21 +0200

    room runner dans common

---
+++ Atelier3/api-common/src/main/java/fr/cpe/scoobygang/common/runner/RoomApplicationRunner.java
@@ -0,0 +1,43 @@
+package fr.cpe.scoobygang.common.runner;
+
+import fr.cpe.scoobygang.common.model.Room;
+import fr.cpe.scoobygang.common.model.User;
+import fr.cpe.scoobygang.common.repository.RoomRepository;
+import fr.cpe.scoobygang.common.repository.UserRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@Order(4)
+public class RoomApplicationRunner implements ApplicationRunner {
+    private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
+
+    public RoomApplicationRunner(RoomRepository roomRepository, UserRepository userRepository) {
+        this.roomRepository = roomRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (roomRepository.count() > 0)
+            return;
+
+       List<User> users = new ArrayList<>();
+       Iterable<User> usersIterable = userRepository.findAll();
+
+       usersIterable.forEach(users::add);
+
+       Collections.shuffle(users);
+       users = users.stream().limit(5).toList();
+
+       List<Room> rooms = users.stream().map(Room::random).toList();
+       roomRepository.saveAll(rooms);
+    }
+}
